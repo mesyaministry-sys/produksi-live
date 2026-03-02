7e3379f3b33cf4e0059cb6116eb049db2f988f5b
mesyaministry-sys <mesyaministry@gmail.com>
2026-03-02 09:31:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ if not st.session_state['logged_in']:
 DAFTAR_FILE = {
     "Januari 2026": "1MQsvhmWmrGNtp3Txh07Z-88VfgEZTj_WBD5zLNs9GGY",  
     "Februari 2026": "1Gy7h6ZUw2i-JoNFZLN7t7M8Ir1bsN2mLyfizK4zjnDA",              
-    "Maret 2026": "MASUKKAN_ID_SHEET_MARET_DISINI",                    
+    "Maret 2026": "1PdQn2ukPf6If5DnoETN7cjGeerPVoT5gOIZOZ6-FjLA",                    
 }
 
 # ==========================================
@@ -405,3 +405,4 @@ try:
 except Exception as e:
     # Tangkap error umum tapi tampilkan sebagai warning 'Belum terinput'
     st.warning(f"⚠️ Data belum terinput atau format belum sesuai.")
+
